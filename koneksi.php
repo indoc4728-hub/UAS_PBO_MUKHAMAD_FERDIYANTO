dc73e3d9d6700b470f5f70f5cf6a48ef0f48287e
indoc4728-hub <indoc4728@gmail.com>
2026-06-26 08:23:54 +0700

    tahap 3 implementasi abstrak

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,35 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "db_uas_pbo_trpl1a_mukhamad_ferdiyanto";
+class Koneksi
+{
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $database = "db_uas_pbo_trpl1a_mukhamad_ferdiyanto";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+    public $conn;
 
-if (!$conn) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+    public function __construct()
+    {
+        $this->conn = new mysqli(
+            $this->host,
+            $this->user,
+            $this->password,
+            $this->database
+        );
+
+        if ($this->conn->connect_error) {
+            die("Koneksi Gagal : " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
 }
+
+$koneksi = new Koneksi();
+$conn = $koneksi->getConnection();
+
 ?>
